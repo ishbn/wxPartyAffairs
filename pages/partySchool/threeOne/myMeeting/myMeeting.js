501170270de95285ad59ae625ce54c1d9f7b198e
hbn <hyqhbn@gmail.com>
2019-01-20 23:37:46 +0800

    会议列表页面优化

--- pages/partySchool/threeOne/myMeeting/myMeeting.js
+++ pages/partySchool/threeOne/myMeeting/myMeeting.js
@@ -1,16 +1,27 @@
 // pages/partySchool/threeOne/myMeeting/myMeeting.js
+var commonUtils = require("../../../../utils/commonUtil.js");
+var paValidUtil = require("../../../../utils/paValidUtil.js");
+var pahelper = require("../../../../utils/pahelper.js");
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    thisPageUrl :"/pages/partySchool/threeOne/myMeeting/myMeeting",
     menu: "/images/partySchool_icon/menu.png", //菜单图标
+    notStartIcon:"/images/partySchool_icon/willJoin.png",
+    endedIcon:"/images/partySchool_icon/null.png",
+    showMenuIcom:"/images/partySchool_icon/menu.png",
+    hiddenMenuIcon:"/images/partySchool_icon/menu1.png",
     open: false, //下拉框的状态
     clear: true,//清除icon的状态
     inputVal: '',//输入框的值
     isFocus:false, //输入框是否获取焦点
     currentTab: 0, //预设当前项的值
+    pageNum:0,
+    pageLength: 10,
+    totalPageNum:1,
     willMeetings:[
       {
         title:'会议标题',
@@ -33,94 +44,32 @@ Page({
     ]
   
   },
-  //输入框显示清除按键
-  showClear: function (e) {
-    var value = e.detail.value;
-    if (value != '') {
-      this.setData({
-        inputVal: value,
-        clear: false
-      })
-    } else {
-      this.setData({
-        clear: true
-      })
-    }
-  },
-  //清除输入框中的内容
-  clearVal: function () {
-    this.setData({
-      inputVal: '',
-      clear: true
-    })
-  },
-  //输入框获取或失去焦点
-  focus: function(e){
-    if(this.data.inputVal=='')
-    this.setData({
-      isFocus: true
-    })
-  },
-  //取消输入
-  cancel: function (e) {
-    this.setData({
-      isFocus: false,
-      inputVal:'',
-      clear:true
-    })
-  },
-  //点击切换
-  clickTab: function (e) {
-    var that = this;
-    that.setData({
-      currentTab: e.target.dataset.current
-    });
-  },
-  //滑动切换
-  swiperTab: function (e) {
-    var that = this;
-    that.setData({
-      currentTab: e.detail.current
-    });
-  },
-  //显示下拉框
-  showitem: function () {
-    this.setData({
-      open: !this.data.open
-    });
-    if (this.data.menu === "/images/partySchool_icon/menu.png") {
-      this.setData({
-        menu: "/images/partySchool_icon/menu1.png"
-      })
-    } else {
-      this.setData({
-        menu: "/images/partySchool_icon/menu.png"
-      })
-    }
-  },
-  //点击蒙层恢复
-  hiddenShadow: function () {
-    this.setData({
-      open: !this.data.open
-    });
-    if (this.data.menu === "/images/partySchool_icon/menu.png") {
-      this.setData({
-        menu: "/images/partySchool_icon/menu1.png"
-      })
-    } else {
-      this.setData({
-        menu: "/images/partySchool_icon/menu.png"
-      })
-    }
-  },
-
+ 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that = this;
+    if (!paValidUtil.checkLogin(that.data.thisPageUrl,1)){
+      return;
+    }
+    var url = "meetingMenu_1/" + (that.data.pageNum + 1) + "/" + that.data.pageLength;
+    commonUtils.commonAjax(url,"",1).then(that.processData);
   },
-
+  processData:function(res){
+      var that = this;
+      console.log(res);
+    if (res.statusCode==200 && res.data.status == 0){
+      var list = paValidUtil.checkImgPath(res.data.data.list);
+        that.setData({
+          willMeetings: list,
+          pageNum: res.data.data.pageNum,
+          totalPageNum: res.data.data.totalPageNum
+        });
+      }else{
+      commonUtils.commonTips(res.statusCode);
+      }
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -168,5 +117,88 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  //输入框显示清除按键
+  showClear: function (e) {
+    var value = e.detail.value;
+    if (value != '') {
+      this.setData({
+        inputVal: value,
+        clear: false
+      })
+    } else {
+      this.setData({
+        clear: true
+      })
+    }
+  },
+  //清除输入框中的内容
+  clearVal: function () {
+    this.setData({
+      inputVal: '',
+      clear: true
+    })
+  },
+  //输入框获取或失去焦点
+  focus: function (e) {
+    if (this.data.inputVal == '')
+      this.setData({
+        isFocus: true
+      })
+  },
+  //取消输入
+  cancel: function (e) {
+    this.setData({
+      isFocus: false,
+      inputVal: '',
+      clear: true
+    })
+  },
+  //点击切换
+  clickTab: function (e) {
+    var that = this;
+    that.setData({
+      currentTab: e.target.dataset.current
+    });
+  },
+  //滑动切换
+  swiperTab: function (e) {
+    var that = this;
+    that.setData({
+      currentTab: e.detail.current
+    });
+  },
+  //显示下拉框
+  showitem: function () {
+    var that = this;
+    that.setData({
+      open: !that.data.open
+    });
+    if (that.data.menu === that.data.showMenuIcom) {
+      that.setData({
+        menu: that.data.hiddenMenuIcon
+      })
+    } else {
+      that.setData({
+        menu: that.data.showMenuIcom
+      })
+    }
+  },
+  //点击蒙层恢复
+  hiddenShadow: function () {
+    var that = this;
+    that.setData({
+      open: !that.data.open
+    });
+    if (that.data.menu === that.data.showMenuIcom) {
+      that.setData({
+        menu: that.data.hiddenMenuIcon
+      })
+    } else {
+      that.setData({
+        menu: that.data.showMenuIcom
+      })
+    }
   }
+
 })
